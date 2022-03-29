9c8f18bfafd46c8e700ab7c6e98749aa2e1a01a4
Bojan Dabevski <74015131+BojanDabevski@users.noreply.github.com>
2022-03-29 23:57:17 +0200

    websocket

---
+++ src/main/java/com/example/websockettutorial/WebSocketTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.websockettutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSocketTutorialApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/websockettutorial/model/MessageType.java
@@ -0,0 +1,7 @@
+package com.example.websockettutorial.model;
+
+public enum MessageType {
+    CHAT,
+    CONNECT,
+    DISCONNECT
+}
